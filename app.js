46bc47b60f96834ee61a7847c0488493f8632525
sultan <sultanjarrar89@gmail.com>
2021-01-12 18:34:52 -0800

    Cookies

--- app.js
+++ app.js
@@ -28,37 +28,49 @@ connect.then(
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
-function auth(req, res, next) {
-  console.log(req.headers)
-  const authHeader = req.headers.authorization
-  if (!authHeader) {
-    const err = new Error('You are not authenticated!')
-    res.setHeader('WWW-Authenticate', 'Basic')
-    err.status = 401
-    return next(err)
-  }
 
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64')
-    .toString()
-    .split(':')
-  const user = auth[0]
-  const pass = auth[1]
-  if (user === 'admin' && pass === 'password') {
-    return next() // authorized
-  } else {
-    const err = new Error('You are not authenticated!')
-    res.setHeader('WWW-Authenticate', 'Basic')
-    err.status = 401
-    return next(err)
-  }
-}
-
-app.use(auth)
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(cookieParser('12345-67890-09876-54321'))
+app.use(auth)
+
 app.use(express.static(path.join(__dirname, 'public')))
+function auth(req, res, next) {
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!')
+      res.setHeader('WWW-Authenticate', 'Basic')
+      err.status = 401
+      return next(err)
+    }
+
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64')
+      .toString()
+      .split(':')
+    const user = auth[0]
+    const pass = auth[1]
+
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', { signed: true })
+      return next() // authorized
+    } else {
+      const err = new Error('You are not authenticated!')
+      res.setHeader('WWW-Authenticate', 'Basic')
+      err.status = 401
+      return next(err)
+    }
+  } else {
+    if (req.signedCookies.user === 'admin') {
+      return next()
+    } else {
+      const err = new Error('You are not authenticated!')
+      err.status = 401
+      return next(err)
+    }
+  }
+}
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
