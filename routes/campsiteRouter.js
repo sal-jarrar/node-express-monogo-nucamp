7f0b5ad3450fcfd09c261b35a0e9c7be21ed33b5
sultan <sultanjarrar89@gmail.com>
2021-01-14 00:47:37 -0800

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -8,6 +8,7 @@ campsiteRouter
   .route('/')
   .get((req, res, next) => {
     Campsite.find()
+      .populate('comments.author')
       .then((campsites) => {
         res.statusCode = 200
         res.setHeader('Content-Type', 'application/json')
@@ -43,6 +44,7 @@ campsiteRouter
   .route('/:campsiteId')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then((campsite) => {
         res.statusCode = 200
         res.setHeader('Content-Type', 'application/json')
@@ -84,6 +86,7 @@ campsiteRouter
   .route('/:campsiteId/comments')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then((campsite) => {
         if (campsite) {
           res.statusCode = 200
@@ -97,10 +100,11 @@ campsiteRouter
       })
       .catch((err) => next(err))
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
+          req.body.author = req.user._id
           campsite.comments.push(req.body)
           campsite
             .save()
@@ -118,13 +122,13 @@ campsiteRouter
       })
       .catch((err) => next(err))
   })
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403
     res.end(
       `PUT operation not supported on /campsites/${req.params.campsiteId}/comments`
     )
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
@@ -152,6 +156,7 @@ campsiteRouter
   .route('/:campsiteId/comments/:commentId')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
           res.statusCode = 200
@@ -169,13 +174,13 @@ campsiteRouter
       })
       .catch((err) => next(err))
   })
-  .post((req, res) => {
+  .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403
     res.end(
       `POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`
     )
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
@@ -205,7 +210,7 @@ campsiteRouter
       })
       .catch((err) => next(err))
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
