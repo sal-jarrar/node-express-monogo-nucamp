8ca5b9ea8d761d9dd49c9f0e43a4f3aa442b8d6d
sultan <sultanjarrar89@gmail.com>
2021-01-20 21:10:46 -0800

    CORS

--- routes/users.js
+++ routes/users.js
@@ -3,30 +3,26 @@ const User = require('../models/user')
 const passport = require('passport')
 const authenticate = require('../authenticate')
 const router = express.Router()
+const cors = require('./cors')
 
 /* GET users listing. */
 router.get(
   '/',
+  cors.corsWithOptions,
   authenticate.verifyUser,
   authenticate.verifyAdmin,
   function (req, res, next) {
-    if (req.user.admin) {
-      User.find()
-        .then((users) => {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'application/json')
-          res.json(users)
-        })
-        .catch((err) => next(err))
-    } else {
-      const err = new Error('You are not authorized to perform this operation!')
-      err.statusCode = 403
-      next(err)
-    }
+    User.find()
+      .then((users) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(users)
+      })
+      .catch((err) => next(err))
   }
 )
 
-router.post('/signup', (req, res) => {
+router.post('/signup', cors.corsWithOptions, (req, res) => {
   User.register(
     new User({ username: req.body.username }),
     req.body.password,
@@ -60,18 +56,23 @@ router.post('/signup', (req, res) => {
   )
 })
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
-  const token = authenticate.getToken({ _id: req.user._id })
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.json({
-    success: true,
-    token: token,
-    status: 'You are successfully logged in!',
-  })
-})
+router.post(
+  '/login',
+  cors.corsWithOptions,
+  passport.authenticate('local'),
+  (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id })
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.json({
+      success: true,
+      token: token,
+      status: 'You are successfully logged in!',
+    })
+  }
+)
 
-router.get('/logout', (req, res, next) => {
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
   if (req.session) {
     req.session.destroy()
     res.clearCookie('session-id')
